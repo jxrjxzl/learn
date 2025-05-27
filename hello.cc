0a89c36a7c6895e031230619eccf8ab00d55914a
jxrjxzl <3308553384@qq.com>
2025-05-27 21:59:13 +0800

    add one line at main branch

--- hello.cc
+++ hello.cc
@@ -2,6 +2,6 @@
 
 int main() {
     std::cout << "Hello world" << std::endl;
-    std::cout << "adding a line in dev branch" << std::endl;
+    std::cout << "adding one line at main branch" << std::endl;
     return 0;
 }
