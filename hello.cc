6bb88701cffbff7f15d8a4b5b266e4cc63951d82
jxrjxzl <3308553384@qq.com>
2025-05-27 22:48:29 +0800

    change hello.cc for new test

--- hello.cc
+++ hello.cc
@@ -1,11 +1,6 @@
 #include <iostream>
 
 int main() {
-<<<<<<< HEAD
-    std::cout << "hello world\n";
-    std::cout << "has a bug\n";
-=======
     std::cout << "Hello world" << std::endl;
->>>>>>> dev
     return 0;
 }
