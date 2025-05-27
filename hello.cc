a4637efe3ae24c41cdbc41a229a6d34b78837ee1
jxrjxzl <3308553384@qq.com>
2025-05-27 21:58:03 +0800

    add a line at feature1

--- hello.cc
+++ hello.cc
@@ -2,6 +2,6 @@
 
 int main() {
     std::cout << "Hello world" << std::endl;
-    std::cout << "adding a line in dev branch" << std::endl;
+    std::cout << "add one line at feature1 branch" << std::endl;
     return 0;
 }
